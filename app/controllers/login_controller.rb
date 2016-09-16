9499194e61b50518b25e640a5bdc8ba4b37eaeca
Amanda Hinchey & Jenn Su <pair+akhinchey.jennysoup@devbootcamp.com>
2016-09-16 15:12:03 -0400

    Debug login for username and email

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,11 +1,11 @@
-get 'users/login' do
+get '/users/login' do
   erb :'users/login'
 end
 # check erb rout
 
-post 'users/login' do
-  user = User.find_by(username: params[:username])
-  user ||= User.find_by(email: params[:email])
+post '/users/login' do
+  user = User.find_by(username: params[:login])
+  user ||= User.find_by(email: params[:login])
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
     redirect '/'
