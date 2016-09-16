7d4bda80a9258b7a01f348bbc84e4a327a66ee45
Austin Lanari & Jonathan Marchand <pair+foggy1.jmarsh433@devbootcamp.com>
2016-09-16 19:07:02 -0400

    Add deck and card methods to keep track of guess success and correct guesses

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,23 @@
 class Card < ActiveRecord::Base
+  attr_accessor :done
+
   belongs_to :deck
   has_many :guesses
 
   validates :question, presence: true
   validates :answer, presence: true
+
+  def guessed_correctly?
+    self.guesses.last == self.answer
+  end
+
+  def take_it_out
+    @done = true 
+  end
+
+  def done?
+    @done ||= false
+  end
+
+
 end
