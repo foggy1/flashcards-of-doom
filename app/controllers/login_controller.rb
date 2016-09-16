da44296bd5454124c79663f3cfdeba5574f3f6b1
Amanda Hinchey & Jenn Su <pair+akhinchey.jennysoup@devbootcamp.com>
2016-09-16 14:47:25 -0400

    Add login controller

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,15 @@
+get 'users/login' do
+  erb :'login'
+end
+# check erb rout
+
+post 'users/login' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @error = "Invalid login, please try again."
+    erb :'login'
+  end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,8 @@
+get 'users/new' do
+  erb :'register'
+end
+# check erb rout
+
+post 'users/login' do
+
+end
